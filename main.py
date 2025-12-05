e59c69234d7a501d8959ea433bce6b25f63b4a42
Abderrahmane Chahdane <abderrahmane.chahdane@um6p.ma>
2025-12-05 18:01:06 +0100

    added a function to detect project name and languge used

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
+import os
 
 def main():
     print("=> welcome to the cli")
 
 
-if __name__ == "__main__":
-    main()
+def detect_project_and_language():
+    current_directory = os.getcwd()
+    project_name = os.path.basename(current_directory)
+    ccProject, language = project_name.rsplit("-", 1)
+    if not ccProject:
+        raise ValueError("Project name is empty")
+    if not language:
+        raise ValueError("Language is empty")
+    return ccProject, language
 
-    
+
+
+if __name__ == "__main__":
+    try:
+        project, language = detect_project_and_language()
+        print(project, language)
+    except ValueError as e:
+        print(e)
+        exit(1)
